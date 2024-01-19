0a402b76c404d7a6dc2a5a2abd6802ca3b007c38
M0TS <151381549+M0TS@users.noreply.github.com>
2024-01-19 15:22:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const lunarMinerals = ['pyroxene', 'olivine', 'feldspar','ilmenite', 'spinel', 'armalcolite'];
+const lunarMinerals = ['pyroxene', 'olivine', 'feldspar', 'ilmenite', 'spinel', 'armalcolite'];
 const filteredLunarMinerals = lunarMinerals.filter(mineral => mineral.length <= 8);
 console.log(filteredLunarMinerals);
