45ce974a0c8ea55661018cc6f86f597d3daf5d88
M0TS <151381549+M0TS@users.noreply.github.com>
2024-01-19 20:35:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const lunarMinerals = ['pyroxene', 'olivine', 'feldspar', 'ilmenite', 'spinel', 'armalcolite'];
-const filteredLunarMinerals = lunarMinerals.filter(mineral => mineral.length <= 8);
-console.log(filteredLunarMinerals);
+const lunarMinerals = ['pyroxene', 'olivine', 'feldspar','ilmenite', 'spinel', 'armalcolite'];
+//  Array named lunarMinerals containing words representing different types of minerals.
+
+// Filter() method:
+const filterLunarMinerals = lunarMinerals.filter(filterMinerals);
+// The filter method is used on the lunarMinerals array.
+// The filter method creates a new array (filterLunarMinerals) containing only the minerals whose names have a length of 8 characters or more.
+
+console.log(filterLunarMinerals); // Output: [ 'pyroxene', 'feldspar', 'ilmenite', 'armalcolite' ]
+// The console.log statement prints the filtered array to the console.
+
+// Callback function:
+function filterMinerals(element) { 
+  return element.length >= 8;
+} 
+// The filter method needs a callback function to decide which elements to include.
+// The callback function checks if the length of each mineral's name is greater than or equal to 8 characters.
